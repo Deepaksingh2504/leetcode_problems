f9d22534ec2418c3222f2bde263066d126797d85
Deepaksingh2504 <deepaksingh25082004@gmail.com>
2026-06-15 15:42:32 +0530

    Add solution for longest harmonious subsequence

---
+++ Arrays/longestHarmoniousSubsequence.java
@@ -0,0 +1,27 @@
+// Longest Harmonious Subsequence
+
+class Solution {
+    public int findLHS(int[] nums) {
+        
+        HashMap<Integer,Integer>map=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        int longest=0;
+    
+        for(int n :map.keySet())
+        {
+            if(map.containsKey(n+1))
+            {
+                longest=(int)Math.max(longest,map.get(n)+map.get(n+1));
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return longest;
+    }
+}
