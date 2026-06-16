46072bb9ff8342bfddcb68bbcb58e4a9e05a807b
Deepaksingh2504 <deepaksingh25082004@gmail.com>
2026-06-16 20:42:57 +0530

    Add solution for finding the differing character
    Implemented two methods to find the differing character between two strings: one using XOR and another using a frequency array.

--- Strings/findTheDiffrence.java
+++ Strings/findTheDiffrence.java
@@ -1,1 +1,49 @@
+//  FIND THE DIFFRENCE
 
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        char c[]=s.toCharArray();
+        char v[]=t.toCharArray();
+        char res=0;
+        for(int i=0;i<c.length;i++)
+        {
+            res^=c[i];
+        }
+        for(int i=0;i<v.length;i++)
+        {
+            res^=v[i];
+        }
+
+        return res;
+    }
+}
+
+// or using frequency array
+
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        int freq[]=new int[26];
+        for(int i=0;i<t.length();i++)
+        {
+            freq[t.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+             freq[s.charAt(i)-'a']--;
+        }
+
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]>0)
+            {
+                if(freq[i]==1)
+                {
+                    return (char)(i+'a');
+                }
+            }
+        }return ' ';
+    }
+}
