a3b8eb6b2d0aef83285466f09aa6552394414fa7
Deepaksingh2504 <deepaksingh25082004@gmail.com>
2026-06-17 14:36:45 +0530

    Implement reversePrefix method in Solution class

---
+++ Strings/reversePreffixWord.java
@@ -0,0 +1,27 @@
+// REVERSE PREFFIX OF WORD
+
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        
+        int index=0;
+       for(int i=0;i<word.length();i++)
+       {
+        if(word.charAt(i)==ch)
+        {
+            index=i;
+            break;
+        }
+       }
+       StringBuilder sb=new StringBuilder();
+
+        for(int i=index;i>=0;i--)
+       {
+            sb.append(word.charAt(i));
+       }
+       sb.append(word.substring(index+1,word.length()));
+
+       return sb.toString();
+    }
+
+   
+}
