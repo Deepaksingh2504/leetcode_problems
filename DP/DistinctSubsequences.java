c327379453354ccf1ce635f83672a2b27c8bbbbc
Deepaksingh2504 <deepaksingh25082004@gmail.com>
2026-09-13 23:41:51 +0530

    Add solution for distinct subsequences problem
    Implement dynamic programming solution for counting distinct subsequences of string t in string s.

---
+++ DP/DistinctSubsequences.java
@@ -0,0 +1,39 @@
+//Distinct Subsequences
+
+
+  class Solution {
+    public int numDistinct(String s, String t) {
+        
+        int dp[][]=new int[s.length()+1][t.length()+1];
+
+        for(int i=0;i<s.length()+1;i++)
+        {
+            Arrays.fill(dp[i],-1);
+        }
+       return solve(s,t,s.length(),t.length(),dp);
+    }
+
+    int solve(String s,String t,int i,int j,int dp[][])
+    {
+        if(j==0)
+        {
+            return dp[i][j]=1;
+        }
+
+        if(i==0)
+        {
+            return dp[i][j]=0;
+        }
+
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+
+        if(s.charAt(i-1)==t.charAt(j-1))
+        {
+            return dp[i][j]= solve(s,t,i-1,j-1,dp)+solve(s,t,i-1,j,dp);
+        }
+        return dp[i][j]= solve(s,t,i-1,j,dp);
+    }
+}
